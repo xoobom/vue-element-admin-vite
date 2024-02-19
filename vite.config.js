6b504824ef31fbc4f63b17130bf21ba4d22386ac
lifuhai <fuhai.li@gientech.com>
2024-02-19 23:31:44 +0800

    vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,68 @@
+import vue from '@vitejs/plugin-vue2';
+import path from 'path';
+import { defineConfig } from 'vite';
+import commonjs from 'vite-plugin-commonjs';
+import vueJsx from '@vitejs/plugin-vue2-jsx';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import vitePluginRequire from 'vite-plugin-require';
+
+export default defineConfig(({ mode, command }) => {
+  return {
+    base: '/',
+    plugins: [
+      vue(),
+      commonjs(),
+      nodePolyfills(),
+      vitePluginRequire(),
+      vueJsx(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
+        symbolId: 'icon-[name]',
+      }),
+      //修复vite Cannot access 'router' before initialization
+      {
+        name: 'singleHMR',
+        handleHotUpdate({ modules }) {
+          modules.map((m) => {
+            m.importedModules = new Set();
+            m.importers = new Set();
+          });
+          return modules;
+        },
+      },
+    ],
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+    define: {
+      'process.env': {},
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          additionalData: `@import "${path.resolve(__dirname, 'src/styles/variables.module.scss')}";@import "${path.resolve(__dirname, 'src/styles/element-variables.scss')}";`,
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+      extensions: ['.mjs', '.js', '.jsx', '.json', '.vue'],
+    },
+    server: {
+      port: 9527,
+      host: true,
+      open: false,
+      //   proxy: {
+      //     '/api': {
+      //       target: 'http://xxx:30808',
+      //     },
+      //   },
+    },
+  };
+});
